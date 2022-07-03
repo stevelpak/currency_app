d289e04f0f4863a2d5afe1e838b1829025055af6
Steve <stevelpak@gmail.com>
2022-07-03 15:57:19 +0500

    fix mini issues

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:currency/utils/routes.dart';
+import 'package:hive_flutter/hive_flutter.dart';
+import 'models/currency_model.dart';
 
-void main() {
+void main() async {
+  await Hive.initFlutter();
+  Hive.registerAdapter<CurrencyModel>(CurrencyModelAdapter());
   runApp(const MyApp());
 }
 
