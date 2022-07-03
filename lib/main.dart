26435d3cd991cc2df1bfc7075ec66d24c2f02af4
Steve <stevelpak@gmail.com>
2022-07-03 11:17:08 +0500

    updated

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:currency/compare_page.dart';
+import 'package:currency/utils/routes.dart';
 
 void main() {
   runApp(const MyApp());
@@ -10,9 +10,9 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: ComparePage(),
+      onGenerateRoute: (settings) => Routes.generateRoute(settings),
     );
   }
 }
